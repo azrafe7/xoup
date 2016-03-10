a9e87b1ff2f5c300a47dcd971149bf84750900c0
azrafe7 <azrafe7@gmail.com>
2016-03-10 15:26:56 +0100

    add dummy Connection classes

---
+++ src/org/jsoup/Connection.hx
@@ -0,0 +1,45 @@
+package org.jsoup;
+
+//NOTE(az): dummy
+
+
+interface Connection {
+	
+}
+
+/**
+ * A Key Value tuple.
+ */
+interface KeyVal {
+
+	/**
+	 * Update the key of a keyval
+	 * @param key new key
+	 * @return this KeyVal, for chaining
+	 */
+	//NOTE(az): setter
+	function setKey(key:String):KeyVal;
+
+	/**
+	 * Get the key of a keyval
+	 * @return the key
+	 */
+	//NOTE(az): getter
+	function getKey():String;
+
+	/**
+	 * Update the value of a keyval
+	 * @param value the new value
+	 * @return this KeyVal, for chaining
+	 */
+	//NOTE(az): setter
+	function setValue(value:String):KeyVal;
+
+	/**
+	 * Get the value of a keyval
+	 * @return the value
+	 */
+	//NOTE(az): getter
+	function getValue():String;
+}
+
---
+++ src/org/jsoup/helper/HttpConnection.hx
@@ -0,0 +1,45 @@
+package org.jsoup.helper;
+
+import org.jsoup.Connection;
+import org.jsoup.helper.HttpConnection.KeyVal;
+
+//NOTE(az): dummy
+
+class HttpConnection implements Connection {
+	
+	public function new() { }
+}
+
+
+class KeyVal implements Connection.KeyVal {
+	
+	public var _key:String;
+	public var _val:String;
+	
+	function new(key:String, val:String) {
+		setKey(key);
+		setValue(val);
+	}
+	
+	public static function create(key:String, val:String):KeyVal {
+		return new KeyVal(key, val);
+	}
+	
+	public function getKey():String {
+		return _key;
+	}
+	
+	public function setKey(key:String):KeyVal{
+		_key = key;
+		return this;
+	}
+	
+	public function getValue():String {
+		return _val;
+	}
+	
+	public function setValue(val:String):KeyVal{
+		_val = val;
+		return this;
+	}
+}
