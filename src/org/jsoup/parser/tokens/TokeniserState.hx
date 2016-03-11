96c74b3be5ec9e4c0cd2e9e5109ab5c5452fc6f8
azrafe7 <azrafe7@gmail.com>
2016-03-11 16:05:17 +0100

    add TokeniserState

---
+++ src/org/jsoup/parser/tokens/TokeniserState.hx
@@ -0,0 +1,11 @@
+package org.jsoup.parser.tokens;
+
+//NOTE(az): dummy for now
+class TokeniserState {
+	static public var Data(default, null):TokeniserState = new TokeniserState();
+	static public var nullChar:Int;
+	
+	public function new() { }
+	
+	public function read(a:Dynamic, b:Dynamic) {};
+}
