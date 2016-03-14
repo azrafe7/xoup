d3b786ee879b44bd8ce8ccb0cc52caae20b7dcbe
azrafe7 <azrafe7@gmail.com>
2016-03-14 15:12:35 +0100

    change test functions to be non-static

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -4,6 +4,8 @@ import org.jsoup.nodes.EntitiesTest;
 import utest.Runner;
 import utest.ui.Report;
 
+using unifill.Unifill;
+
 class TestAll {
 	static public function main():Void {
 		var runner = new Runner();
