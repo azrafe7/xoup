6d71e24824c634e188ba639d03eb944ef1847722
azrafe7 <azrafe7@gmail.com>
2016-04-06 00:56:13 +0200

    TestAll: mini-refactor to call specific tests (so they can be isolated/commented out)

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -2,6 +2,7 @@ package;
 
 import org.jsoup.helper.StringUtil;
 import org.jsoup.helper.StringUtilTest;
+import org.jsoup.integration.ParseTest;
 import org.jsoup.nodes.AttributesTest;
 import org.jsoup.nodes.AttributeTest;
 import org.jsoup.nodes.DocumentTest;
@@ -12,10 +13,16 @@ import org.jsoup.nodes.FormElementTest;
 import org.jsoup.nodes.NodeTest;
 import org.jsoup.nodes.TextNodeTest;
 import org.jsoup.parser.AttributeParseTest;
+import org.jsoup.parser.CharacterReaderTest;
+import org.jsoup.parser.TagTest;
+import org.jsoup.parser.TokenQueueTest;
+import org.jsoup.parser.XmlTreeBuilderTest;
 import org.jsoup.select.CssTest;
 import org.jsoup.select.ElementsTest;
 import org.jsoup.select.QueryParserTest;
 import org.jsoup.select.SelectorTest;
+//import org.jsoup.parser.HtmlParserTest;
+import utest.TestFixture;
 import utest.ui.text.PrintReport;
 
 import utest.Runner;
@@ -24,10 +31,30 @@ import utest.ui.Report;
 using unifill.Unifill;
 
 class TestAll {
+	
+	static var runner:Runner = new Runner();
+	
 	static public function main():Void {
 		
-		var runner = new Runner();
+		addHelperTests();
+		addNodesTests();
+		addSelectTests();
+		addParserTests();
+		addParseTests();
 		
+		var report = new PrintReport(runner);
+		runner.run();
+		
+	#if flash
+		flash.system.System.exit(1);
+	#end
+	}
+	
+	static function addHelperTests() {
+		runner.addCase(new StringUtilTest());
+	}
+	
+	static function addNodesTests() {
 		runner.addCase(new EntitiesTest());
 		runner.addCase(new AttributeTest());
 		runner.addCase(new AttributesTest());
@@ -37,20 +64,25 @@ class TestAll {
 		runner.addCase(new ElementTest());
 		runner.addCase(new TextNodeTest());
 		runner.addCase(new FormElementTest());
-		runner.addCase(new StringUtilTest());
-
+	}
+	
+	static function addSelectTests() {
 		runner.addCase(new ElementsTest());
 		runner.addCase(new SelectorTest());
 		runner.addCase(new CssTest());
 		runner.addCase(new QueryParserTest());
+	}
 
+	static function addParserTests() {
 		runner.addCase(new AttributeParseTest());
-		
-		var report = new PrintReport(runner);
-		runner.run();
-		
-		#if flash
-		flash.system.System.exit(1);
-		#end
+		runner.addCase(new TagTest());
+		runner.addCase(new TokenQueueTest());
+		runner.addCase(new XmlTreeBuilderTest());
+		runner.addCase(new CharacterReaderTest());
+		//runner.addCase(new HtmlParserTest());
+	}
+
+	static function addParseTests() {
+		runner.addCase(new ParseTest());
 	}
 }
